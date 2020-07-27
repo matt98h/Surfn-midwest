1e2e4d34933a4b786b01197b59ac24e634895c6a
andrewwestgard <westgaar9629@uwec.edu>
2020-07-27 09:36:15 -0500

    dynamic render the location data and created form

--- public/js/surf.js
+++ public/js/surf.js
@@ -1,4 +1,37 @@
 // Variables and DOM Elements
+var dropdown = document.getElementById("#locationButton1")
+var addComment = document.getElementById("#addComment")
+var userModal = document.getElementById("#userModal")
+var addComment = document.getElementById("#addComment")
+//Dynamically display locations 
+
+
+
+function myFunction() {
+    var x = document.querySelector("#location1");
+    console.log(x);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  console.log(addComment)
+
+//USER INPUT MODAL
+addComment.addEventListener('click', displayModal());
+
+function displayModal() {
+    userModal.style.display = 'block';
+}
+  
+// $(document).on("click", ".solar-button", function(e){
+//     //my function
+//  });
+//when when Button is pressed change display from none to block. when button is clicked again display block to none
+
+
+
 
 //AJAX calls
 //grab all the locations
