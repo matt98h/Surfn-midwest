c15243f1d014ee7bd715ef25fb53fa9f59af60e4
Matthew Huus <matthew.huus3@gmail.com>
2020-07-28 16:51:24 -0500

    working cloudinary connection

---
+++ config/cloudinary.js
@@ -0,0 +1,13 @@
+var cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: 'dfjy06y9i',
+    api_key: '533758495267833',
+    api_secret: 'vLSkULidZnSIVsvkXUbQAukCCMI'
+  })
+  // app.post("/upload", function(req,res,next) {
+  //   cloudninary.uploader.upload
+  //   const file = req.files.photo;
+  //   console.log(file);
+  // })
+
--- public/js/surf.js
+++ public/js/surf.js
@@ -4,47 +4,46 @@ var addComment = document.getElementById("addComment")
 var userModal = document.getElementById("userModal")
 var userLocationSelection = document.getElementById("locationFormSelect")
 var userRadFactorSelection = document.getElementById("radFactorFormSelect")
-var imagePath = document.getElementById('image')
-var comment = document.getElementById('comment')
+var imagePath = document.getElementById("image");
+var comment = document.getElementById('input-comment')
 var formSubmit = document.getElementById('submit')
 var form = document.getElementById('userInputForm')
 // var wisconsin = window.location.$('/wisconsin')
-//Dynamically display locations 
-
+//Dynamically display locations
 console.log(imagePath);
 console.log(userRadFactorSelection);
 function getLocation() {
     console.log(userLocationSelection.options[userLocationSelection.selectedIndex].value);
 }
-function getRadFactor(){
+function getRadFactor() {
     console.log(userRadFactorSelection.options[userRadFactorSelection.selectedIndex].value)
 }
 
 // function getComment () {
 //     console.log(comment.value)
-    
+
 
 
 function toggleLocation() {
     var x = document.querySelector("#location1");
     console.log(x);
     if (x.style.display === "none") {
-      x.style.display = "block";
+        x.style.display = "block";
     } else {
-      x.style.display = "none";
+        x.style.display = "none";
     }
-  }
-  console.log(addComment)
+}
 
-  function displayModal() {
-      var userForm = document.querySelector('#userModal')
-      console.log(userForm);
-      if(userForm.style.display === 'none') {
-          userForm.style.display = 'block';
-      } else {
-          userForm.style.display = 'none'
-      }
-  }
+
+function displayModal() {
+    var userForm = document.querySelector('#userModal')
+    console.log(userForm);
+    if (userForm.style.display === 'none') {
+        userForm.style.display = 'block';
+    } else {
+        userForm.style.display = 'none'
+    }
+}
 
 //USER INPUT MODAL
 // addComment.addEventListener('click', displayModal());
@@ -73,62 +72,66 @@ function toggleLocation() {
 */
 
 //LOGIN FORM 
-form.addEventListener("submit", function(event) {
+
+
+form.addEventListener("submit", function (event) {
     event.preventDefault();
-    var userInput = {
-        locationId: userLocationSelection.options[userLocationSelection.selectedIndex].value,
+    var form = new FormData();
+    form.append("photo", imagePath.files[0], imagePath.value);
+    
+    
+    
 
-        radFactor: userRadFactorSelection.options[userRadFactorSelection.selectedIndex].value,
-
-        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.surfcanarias.com%2Fknow-the-benefits-of-surfing%2F&psig=AOvVaw2PAZC891Oon-y1UbQKEu2y&ust=1595964162091000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCKjI2vmT7uoCFQAAAAAdAAAAABAD',
-
-        comment: comment.value
+    var settings = {
+        "url": "/upload",
+        "method": "POST",
+        "timeout": 0,
+        // "headers": {
+        //     "Cookie": "connect.sid=s%3AciETEpyz_UwOq2kTlhpL5UVwLFXG5eTL.ZD10fQ2OlHWU4BdfPcTOJuNrWB1p%2BlqD%2BrQ%2B3b7Vfh8"
+        // },
+        "processData": false,
+        "mimeType": "multipart/form-data",
+        "contentType": false,
+        "data": form
     };
-    console.log(userInput);
-    console.log('Recieving feedback')
-    $.post('/api/userInput', userInput)
-    .then(response => console.log(response))
-    .catch(err => console.log(err))
+
+    $.ajax(settings).done(function (response) {
+        var responseData = JSON.parse(response)
+         
+    
+        
+
+        var userInput = {
+            LocationId: userLocationSelection.options[userLocationSelection.selectedIndex].value,
+    
+            radFactor: userRadFactorSelection.options[userRadFactorSelection.selectedIndex].value,
+    
+            image: responseData.result.secure_url,
+    
+            comment: comment.value
+        };
+        console.log(userInput);
+        console.log('Recieving feedback')
+        $.post('/api/userInput', userInput)
+            .then(response => console.log(response))
+            .catch(err => console.log(err))
+    })
+   
 })
 
-
-// function userInputFormSubmit (event) {
-//     event.preventDefault();
-//     userData = {
-//         location : userLocationSelection.options[userLocationSelection.selectedIndex].value,
-//         radFactor : userRadFactorSelection.options[userRadFactorSelection.selectedIndex].value,
-//         comment : comment.value
-//     }
-//     console.log(userData);
-// }
-
-// const sendUserInput = (userData) => {
-//     $.ajax({
-//         url: '/api/userInput',
-//         method: 'POST'
-//     }).then()
-// }
-
-
-
-
-//AJAX calls
-//grab all the locations
-
-// get these to show in our console locally
 $(document).ready(function () {
-  
+
     $(userLocationSelection).on('change', getLocation)
     $(userRadFactorSelection).on('change', getRadFactor)
 
-    
+
 
     const getLocations = () => {
         $.ajax({
             url: '/api/locations',
             method: 'GET',
         }).then(data => {
-         //   console.log(data)
+            //   console.log(data)
         })
     }
     // grab all user data 
@@ -137,7 +140,8 @@ $(document).ready(function () {
             url: 'api/user_data',
             method: 'GET'
         }).then(data => {
-          //  console.log(data)
+            //   for (let i = 0; i < data.length; i++) {
+
         })
     }
     // grab forecast data
@@ -150,7 +154,7 @@ $(document).ready(function () {
                 console.log(data[i].swell)
 
             }
-           // console.log(data)
+            // console.log(data)
         })
     }
 
@@ -159,8 +163,20 @@ $(document).ready(function () {
             url: 'api/UserInput',
             method: 'GET'
         }).then(data => {
-           // var parsedData = JSON.parse(data);
-           // console.log(parsedData);
+            // var parsedData = JSON.parse(data);
+            // console.log(parsedData);
+            for (i = 0; i < data.length; i++) {
+                console.log[data[i].comment]
+                if (data[i].LocationId === 1) {
+
+                    var commentSection = $('<div>').addClass('comment-wrapper').html(data[i].comment)
+
+                    $(commentSection).append()
+
+                    $('#comment').append(data[i].comment)
+                }
+
+            }
         })
     }
 
@@ -197,12 +213,12 @@ $(document).ready(function () {
         // getUserData();
         // getForecast();
         // postUserInput();
-        
+        getUserInput();
     }
     init();
 
 
-//window.location.pathname gets the specific path of the file
+    //window.location.pathname gets the specific path of the file
 
 });
 
